fc39b4913aca63eb0696871e8a2cd59204687628
dominikschuessler <17dominik11@gmail.com>
2022-05-14 01:13:52 +0200

    Alle Kommentare in der Klasse übersetzt und teilweise ergänzt.

--- Transaction.java
+++ Transaction.java
@@ -1,56 +1,83 @@
 // Transaction.java
-// Abstract superclass Transaction represents an ATM transaction
+/**
+ * Die Klasse {@code Transaction} repräsentiert eine Transaktion des
+ * Geldautomaten
+ * 
+ * @author Dominik Schüßler (Kommentarübersetzung)
+ */
+public abstract class Transaction {
 
-public abstract class Transaction
-{
-   private int accountNumber; // indicates account involved
-   protected Screen screen; // ATM's screen
-   private BankDatabase bankDatabase; // account info database
+	/** Kontonummer des Benutzers. */
+	private int accountNumber;
 
-   // Transaction constructor invoked by subclasses using super()
-   public Transaction(int userAccountNumber, Screen atmScreen, 
-      BankDatabase atmBankDatabase)
-   {
-      accountNumber = userAccountNumber;
-      screen = atmScreen;
-      bankDatabase = atmBankDatabase;
-   } // end Transaction constructor
+	/** Bildschirm des Bankautomaten. */
+	protected Screen screen;
 
-   // return account number 
-   public int getAccountNumber()
-   {
-      return accountNumber; 
-   } // end method getAccountNumber
+	/** Datenbank mit Accountinformationen. */
+	private BankDatabase bankDatabase;
 
-   // return reference to screen
-   public Screen getScreen()
-   {
-      return screen;
-   } // end method getScreen
+	/**
+	 * Kontruktor initialisiert alle Attribute
+	 * ({@code accountNumber, screen, bankDatabase}) mit Attributwerten.
+	 * 
+	 * 
+	 * @param userAccountNumber
+	 * @param atmScreen
+	 * @param atmBankDatabase
+	 */
+	public Transaction(int userAccountNumber, Screen atmScreen, BankDatabase atmBankDatabase) {
+		accountNumber = userAccountNumber;
+		screen = atmScreen;
+		bankDatabase = atmBankDatabase;
+	}
 
-   // return reference to bank database
-   public BankDatabase getBankDatabase()
-   {
-      return bankDatabase;
-   } // end method getBankDatabase
+	/**
+	 * Gibt Accountnummer der Instanz zurück.
+	 * 
+	 * @return accountNumber
+	 */
+	public int getAccountNumber() {
+		return accountNumber;
+	}
 
-   // perform the transaction (overridden by each subclass)
-   abstract public void execute();
-} // end class Transaction
+	/**
+	 * Gibt eine Referenz zum Display zurück.
+	 * 
+	 * @return screen
+	 */
+	public Screen getScreen() {
+		return screen;
+	}
 
+	/**
+	 * Gibt Referenz zur Datenbank zurück.
+	 * 
+	 * @return bankDatabase
+	 */
+	public BankDatabase getBankDatabase() {
+		return bankDatabase;
+	}
 
+	/**
+	 * Abstrakte Methode, die die Transaktion durchführt. Implementierung duch
+	 * jeweilige Subklasse.
+	 * 
+	 * @see BalanceInquiry
+	 * @see Deposit
+	 * @see Withdrawal
+	 */
+	abstract public void execute();
+}
 
 /**************************************************************************
- * (C) Copyright 1992-2014 by Deitel & Associates, Inc. and               *
- * Pearson Education, Inc. All Rights Reserved.                           *
- *                                                                        *
- * DISCLAIMER: The authors and publisher of this book have used their     *
- * best efforts in preparing the book. These efforts include the          *
- * development, research, and testing of the theories and programs        *
- * to determine their effectiveness. The authors and publisher make       *
- * no warranty of any kind, expressed or implied, with regard to these    *
- * programs or to the documentation contained in these books. The authors *
- * and publisher shall not be liable in any event for incidental or       *
- * consequential damages in connection with, or arising out of, the       *
- * furnishing, performance, or use of these programs.                     *
+ * (C) Copyright 1992-2014 by Deitel & Associates, Inc. and * Pearson Education,
+ * Inc. All Rights Reserved. * * DISCLAIMER: The authors and publisher of this
+ * book have used their * best efforts in preparing the book. These efforts
+ * include the * development, research, and testing of the theories and programs
+ * * to determine their effectiveness. The authors and publisher make * no
+ * warranty of any kind, expressed or implied, with regard to these * programs
+ * or to the documentation contained in these books. The authors * and publisher
+ * shall not be liable in any event for incidental or * consequential damages in
+ * connection with, or arising out of, the * furnishing, performance, or use of
+ * these programs. *
  *************************************************************************/
