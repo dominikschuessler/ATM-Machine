76167039317a1072bb361f75cda6e47d88252614
Cookie-Design <kerstin.huebner@yahoo.de>
2022-05-15 00:24:41 +0200

    Uebersetzung

--- Iterator.java
+++ Iterator.java
@@ -1,6 +1,6 @@
 
 public interface Iterator {
-	//This is the interface for the Iterator pattern.
+	//Dies ist die Schnittstelle für das Iterator pattern.
 		boolean hasNext(int position);
 		Object next(int position);
 		boolean hasPrev(int position);
