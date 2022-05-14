4c9fcabdff04d77bce6e861fcb27d0d090fb41aa
Cookie-Design <kerstin.huebner@yahoo.de>
2022-05-15 00:26:11 +0200

    Uebersetzung

--- AccountFactory.java
+++ AccountFactory.java
@@ -1,7 +1,7 @@
 
 public class AccountFactory extends Account {
 
-   //This code is used for creating additional accounts to add to the database.
+   //Dieser Code wird für die Erstellung zusätzlicher Konten verwendet, die der Datenbank hinzugefügt werden.
 	public AccountFactory(String Username, int theAccountNumber, int thePIN, double theAvailableBalance,
 			double theTotalBalance, int isadmin) {
 		super(Username, theAccountNumber, thePIN, theAvailableBalance, theTotalBalance, isadmin);
