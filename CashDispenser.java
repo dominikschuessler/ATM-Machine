c0425fc56a3b3a12b6d24a4261bb27fafe9fa478
Annalena Maier <annalena.m.00@web.de>
2022-05-14 22:15:19 +0200

    "Kommentar übersetzt"

--- CashDispenser.java
+++ CashDispenser.java
@@ -25,7 +25,7 @@ public class CashDispenser
    // indicates whether cash dispenser can dispense desired amount
    public boolean isSufficientCashAvailable(int amount)
    {
-      int billsRequired = amount / 20; // number of $20 bills required
+      int billsRequired = amount / 20; /** Die Menge an erforderten $20 Scheinen. */
 
       if (count >= billsRequired )
          return true; // enough bills available
