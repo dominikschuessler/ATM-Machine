f68de498f6a119c9e5cc5a88383124015df8f46a
dominikschuessler <17dominik11@gmail.com>
2022-05-15 22:45:33 +0200

    Java Doc konforme Kommentare + Ergänzung
    Migration bestehender Kommentare in solche, die JavaDoc konform sind.
    Ergänzung zusätzlicher Information zu bisher unkommentierten Methoden

--- Keypad.java
+++ Keypad.java
@@ -1,5 +1,6 @@
+
 // Keypad.java
-// Represents the keypad of the ATM
+// Programm nutzt Scanner, um Nutzereingaber zu erfassen
 import java.util.Scanner;
 import java.awt.Button;
 import java.awt.Color;
@@ -14,108 +15,138 @@ import javax.swing.JTextField;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JButton;// program uses Scanner to obtain user input
+import javax.swing.JButton;
 
-public class Keypad 
-{
-   private Scanner input; // liest Daten aus der Befehlszeile
-   private String userinput;
-   public static JButton B1;
-   public static JButton B2;
-   public static JButton B3;
-   public static JButton B4;
-   public static JButton B5;
-   public JButton B6;
-   public JButton B7;
-   public JButton B8;
-   public JButton B9;
-   public JButton B0;
-   public JButton BClear;
-   public JButton BEnter;
-   
-  
-   
-                         
-   // Der Konstruktor ohne Argumente initialisiert den Scanner
-   public Keypad()
-   {
-      input = new Scanner(System.in);    
-   } // ende no-argument Keypad constructor
+/**
+ * Stellt das Eingabefeld des Bankautomaten dar
+ * 
+ * @author Kerstin Huebner
+ * @author Dominik Schuessler
+ * 
+ */
+public class Keypad {
 
-   // Rückgabe eines vom Benutzer eingegebenen Integer-Wertes 
-   public int getInput()
-   {
-      return input.nextInt(); // wir nehmen an, dass der Benutzer eine ganze Zahl eingibt  
-   } // ende methode getInput
-  
-   public void setbuttons(){
-	   
-	   B1 = new JButton("1");
-	   B1.setText("1");
-	   B2 = new JButton("2");
-	   B3 = new JButton("3");
-	   B4 = new JButton("4");
-	   B5 = new JButton("5");
-	   B6 = new JButton("6");
-	   B7 = new JButton("7");
-	   B8 = new JButton("8");
-	   B9 = new JButton("9");
-	   B0 = new JButton("0");
-	   BClear = new JButton("Clear");
-	   BEnter = new JButton("Enter");
-			   
-   }
-   
-   public JPanel addkeypad(){
-	   JPanel panel = new JPanel();
-	   panel.setPreferredSize(new Dimension(180, 160));
-	   panel.setBackground(Color.gray);
-	   panel.setLayout(new FlowLayout()); 
-	   panel.add(B1);
-	   panel.add(B2);
-	   panel.add(B3);
-	   panel.add(B4);
-	   panel.add(B5);
-	   panel.add(B6);
-	   panel.add(B7);
-	   panel.add(B8);
-	   panel.add(B9);
-	   panel.add(BClear);
-	   panel.add(B0);
-	   panel.add(BEnter);
-	   
-	   return panel;
-   }
-   
-   
-   
-   
-   
-   public String userinput(){
-	   return userinput();
-   }
-   
-   public void resetuserinput(){
-	   userinput = "";
-   }
-   
-} // end class Keypad  
+	/** Liest Daten der Kommandozeile */
+	private Scanner input;
+	private String userinput;
+	
+	/* Buttons des Eingabefeldes
+	 * 
+	 * Warum sind die Buttons 1-5 static und der Rest nicht?
+	 */
+	
+	public static JButton B1;
+	public static JButton B2;
+	public static JButton B3;
+	public static JButton B4;
+	public static JButton B5;
+	public JButton B6;
+	public JButton B7;
+	public JButton B8;
+	public JButton B9;
+	public JButton B0;
+	public JButton BClear;
+	public JButton BEnter;
 
+	/**
+	 * Parameterloser Konstruktor deklariert {@code input} als neues Scanner Objekt.
+	 */
+	public Keypad() {
+		input = new Scanner(System.in);
+	}
 
+	/**
+	 * Gibt die vom Benutzer eingegebene Zahl zurueck.
+	 * 
+	 * Es wird angenommen, dass der Benutzer eine Ganzzahl eingibt.
+	 * 
+	 * @return die nächste Ganzzahl der Benutzereingabe
+	 */
+	public int getInput() {
+		return input.nextInt();
+	} // end method getInput()
 
+	/**
+	 * Erstellt die Tasten des Eingabefeldes.
+	 * 
+	 * Dazu wird fuer jeden Button ein Objekt der Klasse {@code JButton} erzeugt,
+	 * das ueber den entsprechenden Konstruktor mit einem Buttontext initialisiert
+	 * wird.
+	 */
+	public void setbuttons() {
 
+		B1 = new JButton("1");
+		
+		//ueberfluessig weil Konstruktor Beschriftung schon als Parameter aufnimmt
+		B1.setText("1");
+		
+		B2 = new JButton("2");
+		B3 = new JButton("3");
+		B4 = new JButton("4");
+		B5 = new JButton("5");
+		B6 = new JButton("6");
+		B7 = new JButton("7");
+		B8 = new JButton("8");
+		B9 = new JButton("9");
+		B0 = new JButton("0");
+		BClear = new JButton("Clear");
+		BEnter = new JButton("Enter");
+
+	}
+
+	/**
+	 * Erstellt ein neues Eingabefeld mit den Ziffern 0-9, einer Clear und einer
+	 * Enter- Taste. Dazu wird ein Behälter erzeugt, dem die einzelnen Tasten dann
+	 * hinzugefuegt werden.
+	 * 
+	 * @return panel Der Behaelter, dem die Tasten hinzugefuegt wurden.
+	 */
+	public JPanel addkeypad() {
+		JPanel panel = new JPanel();
+		panel.setPreferredSize(new Dimension(180, 160));
+		panel.setBackground(Color.gray);
+		panel.setLayout(new FlowLayout());
+		panel.add(B1);
+		panel.add(B2);
+		panel.add(B3);
+		panel.add(B4);
+		panel.add(B5);
+		panel.add(B6);
+		panel.add(B7);
+		panel.add(B8);
+		panel.add(B9);
+		panel.add(BClear);
+		panel.add(B0);
+		panel.add(BEnter);
+
+		return panel;
+	}
+
+	/**
+	 * @return Methode ruft sich selbst auf.
+	 */
+	public String userinput() {
+		return userinput();
+	}
+
+	/**
+	 * Setzt die Benutzereingabe auf einen leeren String zurueck.
+	 */
+	public void resetuserinput() {
+		userinput = "";
+	}
+
+} // end class Keypad
 
 /**************************************************************************
- * (C) Copyright 1992-2014 by Deitel & Associates, Inc. and               *
- * Pearson Education, Inc. All Rights Reserved.                           *
- *                                                                        *
- * DISCLAIMER: The authors and publisher of this book have used their     *
- * best efforts in preparing the book. These efforts include the          *
- * development, research, and testing of the theories and programs        *
- * to determine their effectiveness. The authors and publisher make       *
- * no warranty of any kind, expressed or implied, with regard to these    *
- * programs or to the documentation contained in these books. The authors *
- * and publisher shall not be liable in any event for incidental or       *
- * consequential damages in connection with, or arising out of, the       *
- * furnishing, performance, or use of these programs.                     *
+ * (C) Copyright 1992-2014 by Deitel & Associates, Inc. and * Pearson Education,
+ * Inc. All Rights Reserved. * * DISCLAIMER: The authors and publisher of this
+ * book have used their * best efforts in preparing the book. These efforts
+ * include the * development, research, and testing of the theories and programs
+ * * to determine their effectiveness. The authors and publisher make * no
+ * warranty of any kind, expressed or implied, with regard to these * programs
+ * or to the documentation contained in these books. The authors * and publisher
+ * shall not be liable in any event for incidental or * consequential damages in
+ * connection with, or arising out of, the * furnishing, performance, or use of
+ * these programs. *
  *************************************************************************/
