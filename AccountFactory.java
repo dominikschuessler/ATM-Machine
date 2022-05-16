5d90f03cb431c24bace0ebad086c197bdfb5a0f5
dominikschuessler <17dominik11@gmail.com>
2022-05-16 12:44:26 +0200

    Änderungsvorschlag in Klasse AccountFactory
    Die Klasse implementiert lediglich den Konstruktor der Klasse Account,
    der Code ist in diesem Fall redundant, es könnte, solle die Trennung
    beibehalten werden wollen, einfach der super() Konstruktor mit allen
    Parametern aufgerufen werden.
    Der Änderungsvorschlag ist in einem Kommentar hinterlegt und müsste nur
    auskommentiert werde.

--- AccountFactory.java
+++ AccountFactory.java
@@ -1,7 +1,15 @@
 
+/**
+ * Die Klasse {@code AccountFactory} erzeugt neue Instanzen ihrer Superklasse
+ * {@code Account}
+ * 
+ * Kann eventuell durch vorgeschlagenen Kommentar ersetzt werden, da sie lediglich den Konstruktor der Klasse
+ * Account implementiert.
+ * 
+ * @author Dominik Schuessler
+ */
 public class AccountFactory extends Account {
 
-   //Dieser Code wird für die Erstellung zusätzlicher Konten verwendet, die der Datenbank hinzugefügt werden.
 	public AccountFactory(String Username, int theAccountNumber, int thePIN, double theAvailableBalance,
 			double theTotalBalance, int isadmin) {
 		super(Username, theAccountNumber, thePIN, theAvailableBalance, theTotalBalance, isadmin);
@@ -9,8 +17,13 @@ public class AccountFactory extends Account {
 		setAccountNumber(theAccountNumber);
 		setPin(thePIN);
 		setAvailableBalance(theAvailableBalance);
-	      setTotalBalance(theTotalBalance);
-	      setAdmin(isadmin);
-	}
+		setTotalBalance(theTotalBalance);
+		setAdmin(isadmin);
 	}
 
+//	public AccountFactory(String username, int accountNumber, int PIN, double availabeBalance, double totalBalance,
+//			int admin) {
+//		super(username, accountNumber, PIN, availabeBalance, totalBalance, admin);
+//	}
+
+}
