abace423a9de42ad9bdb1299ee1c0451a6f04869
dominikschuessler <17dominik11@gmail.com>
2022-05-17 23:16:46 +0200

    Lösche .class Dateien

--- Iterator.java
+++ Iterator.java
@@ -1,3 +1,4 @@
+import javax.swing.text.Position;
 
 /**
  * 
@@ -9,10 +10,25 @@
  * */
 public interface Iterator {
 	
+	/**
+	 * Ueberprueft, ob ein Index eines iterrierbaren Objektes einen Nachfolger hat.
+	 * @param position Indexwert, der auf einen Nachfolger ueberprueft werden soll
+	 * @return {@code true}, wenn es einen Nachfolger gibt.
+	 */
 	boolean hasNext(int position);
 
+	/**
+	 * Gibt das Objekt an einer bestimmten Stelle zurueck.
+	 * @param position bestimmte Stelle
+	 * @return Objekt an Stelle {@code position}
+	 */
 	Object next(int position);
 
+	/**
+	 * Ueberprueft, ob ein Index eines iterrierbaren Objektes einen Vorgaenger hat.
+	 * @param position Indexwert, der auf ein Vorgaenger ueberprueft werden soll.
+	 * @return {@code true}, wenn es einen Vorgaenger gibt.
+	 */
 	boolean hasPrev(int position);
 
 }
