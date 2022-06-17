d50f871c83bc922bab3fea151f33c0d4ad56b028
Annalena Maier <annalena.m.00@web.de>
2022-06-17 17:24:06 +0200

    Kommentare ergänzt

--- Withdrawal.java
+++ Withdrawal.java
@@ -43,16 +43,15 @@ public class Withdrawal extends Transaction {
 		cashDispenser = atmCashDispenser;
 	} // end Withdrawal constructor
 
+	/*
+	 * 
+	 * Fehlender Code! <ul> <li>Fuer Auszahlung verfuegbarer Betrag, <li>Referenz
+	 * zur Datenbank und zum Display, <li>Wiederholung bis das Geld ausgezahlt wurde
+	 * oder der Benutzer den Vorgang abbricht. </ul>
+	 */
+
 	/**
 	 * Fuehrt Transaktion durch.
-	 * 
-	 * Fehlender Code!
-	 * <ul>
-	 * <li>Fuer Auszahlung verfuegbarer Betrag,
-	 * <li>Referenz zur Datenbank und zum Display,
-	 * <li>Wiederholung bis das Geld ausgezahlt wurde oder der Benutzer den Vorgang
-	 * abbricht.
-	 * </ul>
 	 * 
 	 * @see Transaction
 	 */
@@ -63,10 +62,11 @@ public class Withdrawal extends Transaction {
 	}
 
 	/**
-	 * Fuehrt die Transaktion durch. ueberprueft, ob das Konto des Nutzers ausreichend
-	 * gedeckt ist, der Automat ueber ausreichend Geld verfuegt und ueberwacht, ob
-	 * der Nutzer den Vorgang abbricht. 
-	 * <p><b>Fehlender Code!</b>
+	 * Fuehrt die Transaktion durch. ueberprueft, ob das Konto des Nutzers
+	 * ausreichend gedeckt ist, der Automat ueber ausreichend Geld verfuegt und
+	 * ueberwacht, ob der Nutzer den Vorgang abbricht.
+	 * <p>
+	 * <b>Fehlender Code!</b>
 	 * 
 	 * @param amount Der Betrag, der ausgegeben werden soll.
 	 */
@@ -76,54 +76,39 @@ public class Withdrawal extends Transaction {
 
 		Screen screen = getScreen();
 
-		/** Der Geldbetrag wurde noch nicht ausgegeben. */
 		boolean cashDispensed = false;
 
 		double availableBalance;
-		/**
+		/*
 		 * ueberprueft, ob der Nutzer einen Auszahlbetrag gewaehlt oder abgebrochen hat.
 		 */
 		// if?? weiter unten steht ein end if ohne, dass eins begonnen wurde
 
-		/** Speichert den verfuegbaren Kontostand */
 		availableBalance = bankDatabase.getAvailableBalance(getAccountNumber());
 
-		/** Ueberprueft, ob der Benutzeraccount ausreichend gedeckt ist. */
 		if (amount <= availableBalance) {
 
-			/** Ueberprueft, ob der Geldausgabeschacht ueber ausreichende Geld verfuegt. */
 			if (cashDispenser.isSufficientCashAvailable(amount)) {
 
-				/** Aktualisiert den beteiligten Account mit Auszahlung. */
 				bankDatabase.debit(getAccountNumber(), amount);
 
-				cashDispenser.dispenseCash(amount); // dispense cash
-				cashDispensed = true; // cash was dispensed
+				cashDispenser.dispenseCash(amount);
+				cashDispensed = true;
 
-				/** Aufforderung an den Benutzer, das Geld zu entnehmen. */
-				screen.messageJLabel7.setText("\nIhr Bargeld wurde" + " ausgezahlt. Bitte entnehmen Sie ihr Bargeld jetzt.");
-			} // end if
-
-			/**
-			 * Im Fall, dass der Geldausgeber nicht ueber eine ausreichende Geldmenge
-			 * verfuegt.
-			 */
-			else
 				screen.messageJLabel7
-						.setText("\nUnzureichender Betrag an Bargeld im Geldautomaten. " + "\n\nBitte geben Sie einen kleineren Betrag an.");
-		} // end if
+						.setText("\nIhr Bargeld wurde" + " ausgezahlt. Bitte entnehmen Sie ihr Bargeld jetzt.");
+			}
 
-		/**
-		 * Im Fall, dass der Benutzeraccount nicht ausreichend gedeckt ist.
-		 */
+			else
+				screen.messageJLabel7.setText("\nUnzureichender Betrag an Bargeld im Geldautomaten. "
+						+ "\n\nBitte geben Sie einen kleineren Betrag an.");
+		}
+
 		else {
-			screen.messageJLabel7
-					.setText("\nUnzureichende Geldmittel auf ihrem Konto." + "\n\nBitte geben Sie einen kleineren Betrag an.");
-		} // end else
-	} // end if
-		// end else
-
-	// end method execute
+			screen.messageJLabel7.setText(
+					"\nUnzureichende Geldmittel auf ihrem Konto." + "\n\nBitte geben Sie einen kleineren Betrag an.");
+		}
+	}
 
 	/**
 	 * Stellt ein Menue mit unteschiedlichen Auszahlbetraegen und der Option zum
@@ -133,60 +118,51 @@ public class Withdrawal extends Transaction {
 	 */
 	private void displayMenuOfAmounts() {
 
-		/** Speichert den Betrag, der zurueckgegeben werden soll. */
 		int userChoice = 0;
 
-		/** Screen Referenz */
 		Screen screen = getScreen();
-		
-		/**Auszahlmenue wird Erzeugt**/
+
 		screen.createWithdrawGUI();
 		screen.Mainframe.add(keypad.addkeypad(), BorderLayout.CENTER);
-		
-		/**Erzeugt neue Instanz von withdraw1**/
+
 		withdraw1 check1 = new withdraw1();
-		
-		/**Erzeugt neue Instanz von withdraw2**/
+
 		withdraw2 check2 = new withdraw2();
-		
-		/**Erzeugt neue Instanz von withdraw3**/
+
 		withdraw3 check3 = new withdraw3();
-		
-		/**Erzeugt neue Instanz von withdraw4**/
+
 		withdraw4 check4 = new withdraw4();
-		
-		/**Erzeugt neue Instanz von withdraw5**/
+
 		withdraw5 check5 = new withdraw5();
-		
-		/**Prueft in {@link Keypad} ob B1 gedrueckt wurde und fuehrt somit check1 aus**/
+
 		Keypad.B1.addActionListener(check1);
-		
-		/**Prueft in {@link Keypad} ob B2 gedrueckt wurde und fuehrt somit check2 aus**/
+
 		Keypad.B2.addActionListener(check2);
-		
-		/**Prueft in {@link Keypad} ob B3 gedrueckt wurde und fuehrt somit check3 aus**/
+
 		Keypad.B3.addActionListener(check3);
-		
-		/**Prueft in {@link Keypad} ob B4 gedrueckt wurde und fuehrt somit check4 aus**/
+
 		Keypad.B4.addActionListener(check4);
-		
-		/**Prueft in {@link Keypad} ob B5 gedrueckt wurde und fuehrt somit check5 aus**/
+
 		Keypad.B5.addActionListener(check5);
-		
-		/**Setzt {@link Screen#Mainframe} zurueck basierend auf den neuen Geldbetraegen**/
+
 		screen.Mainframe.revalidate();
 	}
+
 	/**
-	 * Fuehrt die Transaktion für die erste Auszahlungsm�glichkeit durch 
+	 * Eventklasse, die bei einem ActionEvent die Methode {@code transaction} mit
+	 * dem Betrag von 20 Euro ausführt.
+	 *
 	 */
 	public class withdraw1 implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
 			transaction(20);
 		}
 	}
-	
+
 	/**
-	 * Fuehrt die Transaktion für die zweite Auszahlungsmoeglichkeit durch 
+	 * Eventklasse, die bei einem ActionEvent die Methode {@code transaction} mit
+	 * dem Betrag von 40 Euro ausführt.
+	 *
 	 */
 	public class withdraw2 implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
@@ -195,24 +171,31 @@ public class Withdrawal extends Transaction {
 	}
 
 	/**
-	 * Fuehrt die Transaktion für die dritte Auszahlungsmoeglichkeit durch 
+	 * Eventklasse, die bei einem ActionEvent die Methode {@code transaction} mit
+	 * dem Betrag von 60 Euro ausführt.
+	 *
 	 */
 	public class withdraw3 implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
 			transaction(60);
 		}
 	}
-	
+
 	/**
-	 * Fuehrt die Transaktion für die vierte Auszahlungsmoeglichkeit durch
+	 * Eventklasse, die bei einem ActionEvent die Methode {@code transaction} mit
+	 * dem Betrag von 100 Euro ausführt.
+	 *
 	 */
 	public class withdraw4 implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
 			transaction(100);
 		}
 	}
+
 	/**
-	 * Fuehrt die Transaktion für die fünfte Auszahlungsmoeglichkeit durch 
+	 * Eventklasse, die bei einem ActionEvent die Methode {@code transaction} mit
+	 * dem Betrag von 200 Euro ausführt.
+	 *
 	 */
 	public class withdraw5 implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
